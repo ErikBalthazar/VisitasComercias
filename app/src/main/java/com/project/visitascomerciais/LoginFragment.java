f3aa4e44c3c3655181dad256871f13b72c2d6d7f
ErikBalthazar <erik.balthazar@gmail.com>
2016-02-07 01:22:28 -0200

    Adding registration screen and refactoring some names

--- app/src/main/java/com/project/visitascomerciais/LoginFragment.java
+++ app/src/main/java/com/project/visitascomerciais/LoginFragment.java
@@ -1,14 +1,18 @@
 package com.project.visitascomerciais;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
+
+    private TextView mRegister_button;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -17,8 +21,23 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mRegister_button = (TextView) rootView.findViewById(R.id.sign_up_login_button);
+        mRegister_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistrationActivity();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void openRegistrationActivity() {
+        Intent intent = new Intent(this.getActivity(), RegisterActivity.class);
+        startActivity(intent);
     }
 
 }
